d81349e3fdf9dec087825ff60f337bdadcab17b9
Reed Lathrop <reedlathrop@gmail.com>
2025-11-06 13:37:45 -0600

    Remove dist from folder repo

--- dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const PORT = process.env.PORT || 5001;
-app_1.server.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
-});
